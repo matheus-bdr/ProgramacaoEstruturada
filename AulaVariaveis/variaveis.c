c8a8d48f31052ad52f286e29001f7be21b36f705
matheus-bdr <mmatheuscruzs@gmail.com>
2023-08-08 21:10:09 -0300

    commit

--- AulaVariaveis/variaveis.c
+++ AulaVariaveis/variaveis.c
@@ -1,6 +1,15 @@
+//variavel = nome + endereço + conteúdo 
+//tipos primitivos: int %d, char %s %c, float %f, double float %lf 
 #include <stdio.h>
-
 main() {
-   printf("Hello World");
+
+   int num =0;
+   printf("\n variavel num tem o endereco %d", &num);
+   printf("\n variavel num tem o valor %d", num);
+
+   printf("\n insira seu numero: ");
+   scanf("%d",&num);
+   
+   printf("\n valor inserido %d" ,num);
    return 0;
 }
