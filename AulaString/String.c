3d7a52053c9ade9f19c92a045f9c0f9dc9cc5227
matheus-bdr <mmatheuscruzs@gmail.com>
2023-08-14 19:43:39 -0300

    inicioDeAula

---
+++ AulaString/String.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main()
+{
+
+    int num;
+      printf("\ninsira seu numero: ");
+    scanf("%d", &num);
+
+    printf("O numero digitado foi: %d", num);
+}
---
+++ AulaVariaveis/tipoCharString.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ um array = (0,1,2,3,4,5)
+
+ nome[7] = _ _ _ _ _ _ _
+
+ apontamento a espaçamento em memória.
+*/
+main()
+{
+
+    char nome[50];
+
+    printf("insira aqui um nome: ");
+    scanf("%s",&nome);
+
+    printf("nome: %s" , nome);
+}
