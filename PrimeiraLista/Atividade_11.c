2e23da3e902d65b3378c0449cb508fecd36116a3
matheus-bdr <mmatheuscruzs@gmail.com>
2023-08-22 15:44:01 -0300

    commit

--- PrimeiraLista/Atividade_11.c
+++ PrimeiraLista/Atividade_11.c
@@ -7,6 +7,14 @@
   o valor total de suas vendas, o salário fixo e o valor que ele recebe por 
   carro vendido. Calcule e escreva o salário final do vendedor. */
 main() {
-   
+   float salario, valorDasVendas,salarioFinal;
+  printf("Insira o salario: ");
+  scanf("%f", &salario);
+  printf("Insira o valor vendido no mes: ");
+  scanf("%f", &valorDasVendas);
+
+  valorDasVendas = (valorDasVendas*0.05);
+  salarioFinal = salario + valorDasVendas;
+  printf("O seu saario com a comissao eh: %.2f", salarioFinal);
    return 0;
 }
