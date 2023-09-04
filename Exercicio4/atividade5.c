43b556ea3072c97b122fa08def7afc834fdd2949
matheus-bdr <mmatheuscruzs@gmail.com>
2023-09-04 20:59:23 -0300

    commi

--- Exercicio4/atividade5.c
+++ Exercicio4/atividade5.c
@@ -17,7 +17,7 @@ main()
         printf("numero igual a 200");
     }else if(numero==400){
         printf("numero igual a 400");
-    }else if(numero>=500 && numero<=1000){
+    }else if(numero>500 && numero<=1000){
         printf("intervalo entre 500 e 1000, ou se ela está fora dos escopos anteriores.");
     }else{
         printf("Nao se encaixa nas alternativas");
