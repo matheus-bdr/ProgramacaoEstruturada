d41b86854a99e5e503c916b3c624019874337533
matheus-bdr <mmatheuscruzs@gmail.com>
2023-10-03 16:26:16 -0300

    xommit

--- listaVetores/Atividade_12.c
+++ listaVetores/Atividade_12.c
@@ -2,17 +2,34 @@
 #include <math.h>
 main()
 {
-   int vetor[15], i, somaDasNotas = 0;
+   int vetor[5], i, valoresNegativos = 0, soma = 0, maior = 0, menor;
    float media;
-
    printf("\n-----------Atribuindo valores com o for e printando--------- \n");
-   for (i = 0; i < 15; i++)
+
+   for (i = 0; i < 5; i++)
    {
-      printf("Insira a nota do capeao: ");
+      printf("Insira o primeiro valor: ");
       scanf("%d", &vetor[i]);
-      somaDasNotas = somaDasNotas + vetor[i];
+      if (vetor[i] > maior)
+      {
+         maior = vetor[i];
+      }
+      if (vetor[i] < menor)
+      {
+         menor = vetor[i];
+      }
+      soma = soma + vetor[i];
    }
-   media = somaDasNotas / 15;
-   printf("A media geral eh igual a: %.2f", media);
+
+   media = soma/5;
+
+   for (i = 0; i < 5; i++)
+   {
+      printf("\nO valor negativo na posicao %d eh igual a: %d", i, vetor[i]);
+   }
+   printf("\nO maior valor: %d", maior);
+   printf("\nO menor valor: %d", menor);
+   printf("\na media dos valores: %.2f", media);
+
    return 0;
 }
--- listaVetores/Atividade_13.c
+++ listaVetores/Atividade_13.c
@@ -2,17 +2,33 @@
 #include <math.h>
 main()
 {
-   int vetor[15], i, somaDasNotas = 0;
-   float media;
+   int vetor[5], i, valoresNegativos = 0, soma = 0, maior = 0, menor;
 
    printf("\n-----------Atribuindo valores com o for e printando--------- \n");
-   for (i = 0; i < 15; i++)
+
+   for (i = 0; i < 5; i++)
    {
-      printf("Insira a nota do capeao: ");
+      printf("Insira o primeiro valor: ");
       scanf("%d", &vetor[i]);
-      somaDasNotas = somaDasNotas + vetor[i];
+      if (vetor[i] > maior)
+      {
+         maior = i;
+      }
+      if (vetor[i] < menor)
+      {
+         menor = i;
+      }
+      soma = soma + vetor[i];
    }
-   media = somaDasNotas / 15;
-   printf("A media geral eh igual a: %.2f", media);
+
+   
+
+   for (i = 0; i < 5; i++)
+   {
+      printf("\nO valor negativo na posicao %d eh igual a: %d", i, vetor[i]);
+   }
+   printf("\nO maior valor se encontra na posicao: %d", maior);
+   printf("\nO menor valor se encontra na posicao: %d", menor);
+
    return 0;
 }
