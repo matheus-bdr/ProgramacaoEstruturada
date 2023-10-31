4b8f65419eb580c0222546f982ff6e24e1a64602
matheus-bdr <mmatheuscruzs@gmail.com>
2023-10-31 19:13:35 -0300

    commit

--- exercicio12/atividade1.c
+++ exercicio12/atividade1.c
@@ -1,30 +1,71 @@
 #include <stdio.h>
 
-int recebeInteiro(int numeroInteiro);
-float recebeFloat(float numeroFloat);
-char recebeTexto(char valorTexto);
+float solicitaTotalGasto(float totalGasto);
+void imprimiOpcoes();
+float escolheOpcao(float opcaoVariavel, float valorRecebido);
+float calculaValorFinal(float opcao, float valorGasto);
+
 
 main()
 {
-    int valor = recebeInteiro(valor);
-    printf("valor do inteiro: %d",valor);
+    float valorTotalGasto = solicitaTotalGasto(valorTotalGasto);
+    imprimiOpcoes();
+    int escolheOpcaoRetorno = escolheOpcao(escolheOpcaoRetorno,valorTotalGasto);
+    float calcula= calculaValorFinal( escolheOpcaoRetorno,  valorTotalGasto);
+    printf("O valor total da sua compra foi: %.2f", calcula);
 }
-//funcoes
-int recebeInteiro(int numeroInteiro)
+
+
+
+// funcoes
+float solicitaTotalGasto(float totalGasto)
 {
-    printf("Insira seu numero inteiro: ");
-    scanf("%d", &numeroInteiro);
-    return numeroInteiro;
+    printf("Insira o valor total gasto: ");
+    scanf("%f", &totalGasto);
+    return totalGasto;
 }
-float recebeFloat(float numeroFloat)
+float escolheOpcao(float opcaoVariavel, float valorRecebido)
 {
-    printf("Insira seu numero Float: ");
-    scanf("%d", &numeroFloat);
-    return numeroFloat;
+    int opcao = opcaoVariavel;
+    do
+    {
+        printf("Insira a sua opcao: ");
+        scanf("%d", &opcao);
+        if(opcao==3 &&valorRecebido<=100){
+            opcao = 10;
+        }
+    } while (opcao < 1 && opcao > 3);
+
+    return opcao;
 }
-char recebeTexto(char valorTexto)
+void imprimiOpcoes()
 {
-    printf("\n Digite um String: ");
-    fgets(valorTexto, 20, stdin);
-    return valorTexto;
+    printf("\n (1) A VISTA ");
+    printf("\n --a vista vc tem 10 por cento de desconto--");
+    printf("\n (2) EM 2 VEZES ");
+    printf("\n --preco da etiqueta--");
+    printf("\n (3) EM 3 A 10 VEZES ");
+    printf("\n --compras acima de 100,00, 3por cento de juros ao mes--");
+}
+float calculaValorFinal(float opcao, float valorGasto)
+{
+    int opcaoEscolhida = opcao, quantidadeDeParcelas;
+    switch (opcaoEscolhida)
+    {
+    case 1:
+        valorGasto = valorGasto - (valorGasto * 0.10);
+        break;
+    case 2:
+        valorGasto = valorGasto;
+        break;
+    case 3:
+        do
+        {
+            printf("Insira aqui a sua quantidade de parcelas: ");
+            scanf("%d", &quantidadeDeParcelas);
+        } while ((quantidadeDeParcelas < 3 && quantidadeDeParcelas > 10)&&valorGasto>100);
+        valorGasto = valorGasto + (valorGasto * 0.3);
+        break;
+    }
+    return valorGasto;
 }
